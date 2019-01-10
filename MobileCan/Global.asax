9aa2575465d6da048bf5c5cf16f400a296b49933
Roba <roba18181@gmail.com>
2019-01-11 07:26:56 +1000

    Repo Initialization

---
+++ MobileCan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MobileCan.MvcApplication" Language="C#" %>
---
+++ MobileCan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobileCan.Startup))]
+namespace MobileCan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
