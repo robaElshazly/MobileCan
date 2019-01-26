b563f080f9bdc6872c489371f4d35fa817d3064c
Roba <roba18181@gmail.com>
2019-01-26 13:24:05 +1000

    adding categories partial view

--- MobileCan.Database/scripts/201926011147-create-category.sql
+++ MobileCan.Database/scripts/201926011147-create-category.sql
@@ -10,4 +10,4 @@ insert into Category (name,CreatedBy,ModifiedBy) values
 ('Mobile','data seed','data seed'),
 ('Tablets','data seed','data seed'),
 ('Accessories','data seed','data seed'),
-('wearables','data seed','data seed')
+('Wearables','data seed','data seed')
