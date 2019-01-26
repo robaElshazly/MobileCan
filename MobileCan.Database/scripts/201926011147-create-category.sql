82207ac07326f606a4603105d9b41e71c749b558
Roba <roba18181@gmail.com>
2019-01-26 12:09:02 +1000

    created category table

---
+++ MobileCan.Database/scripts/201926011147-create-category.sql
@@ -0,0 +1,8 @@
+﻿create table Category(
+Id int identity(1,1) Primary key,
+Name nvarchar(200),
+CreatedBy  nvarchar(200),
+CreatedOn  Datetime default(getDate()),
+ModifiedBy  nvarchar(200),
+ModifiedOn  Datetime default(getDate()),
+)
