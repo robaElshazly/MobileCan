8f8ba2b49e46096d86dab33a16803eca4725c793
Roba <roba18181@gmail.com>
2019-01-30 13:05:12 +1000

    create product and insert into it

---
+++ MobileCan.Database/scripts/201930011138-create-product.sql
@@ -0,0 +1,12 @@
+﻿
+create table Product(
+ProductId int identity(1,1) Primary key,
+CategoryId int NOT NULL ,
+FOREIGN KEY (CategoryId) REFERENCES Category(Id) ,
+Name nvarchar(200),
+CreatedBy  nvarchar(200),
+CreatedOn  Datetime default(getDate()),
+ModifiedBy  nvarchar(200),
+ModifiedOn  Datetime default(getDate()),
+)
+
---
+++ MobileCan.Database/scripts/201930011254-insert-product.sql
@@ -0,0 +1,13 @@
+﻿insert into Product (name,CategoryId,CreatedBy,ModifiedBy) values 
+('iPhone XS','1','data seed','data seed'),
+(' iPhone 8','1','data seed','data seed'),
+('Samsung S9','1','data seed','data seed'),
+('HTC','1','data seed','data seed'),
+('iPad','2','data seed','data seed'),
+('samsung tablet','2','data seed','data seed'),
+('iPhone protectors','2','data seed','data seed'),
+('headphone','3','data seed','data seed'),
+('charger for iphone','3','data seed','data seed'),
+('USB-c Charger ','3','data seed','data seed'),
+('iWatch','4','data seed','data seed'),
+(' Samsung watch','4','data seed','data seed')
