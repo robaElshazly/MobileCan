39e4984cd15369bcc97cb4b144d1f6b3e9bee32e
Ammar <ammaroff@users.noreply.github.com>
2019-02-19 13:05:48 +1000

    upgrade .net framework

--- MobileCan/Models/MobileCanModel.Designer.cs
+++ MobileCan/Models/MobileCanModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Repos\MobileCan\MobileCan\Models\MobileCanModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Repos\MobileCan\MobileCan\Models\MobileCanModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
